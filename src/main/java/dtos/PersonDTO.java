be7562888de3a816b9031fdf1e7d990cec696da9
Foldager94 <cph-cf161@cphbusiness.dk>
2020-10-20 10:59:55 +0200

    projekt klasser tilføjet/fjernet

--- src/main/java/dtos/PersonDTO.java
+++ src/main/java/dtos/PersonDTO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package facades;
+package dtos;
 
 /**
  *
  * @author ckfol
  */
-public class FacadePhone {
+public class PersonDTO {
     
 }
