f65bdc5787b93576ca34cf06b7d8c51e11fba221
Foldager94 <cph-cf161@cphbusiness.dk>
2020-10-20 11:24:04 +0200

    GetPersonByPhone done

--- src/main/java/facades/FacadePerson.java
+++ src/main/java/facades/FacadePerson.java
@@ -5,10 +5,64 @@
  */
 package facades;
 
+import dtos.PersonDTO;
+import entities.Person;
+import entities.Phone;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 /**
  *
  * @author ckfol
  */
 public class FacadePerson {
+    
+    private static FacadePerson instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private FacadePerson() {}
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static FacadePerson getFacadePerson(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new FacadePerson();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    
+    
+    // Find person in DB by PhoneNumber
+    // ToDo: Exception
+    public PersonDTO getPersonByPhone(int phoneNumber){
+        EntityManager em = emf.createEntityManager();
+        try{
+            Phone phoneObject = em.find(Phone.class, phoneNumber);
+            Person person = em.find(Person.class, phoneObject.getPId());
+            if (person == null) {
+                System.out.println("Person ikke fundet på telefonnummer");
+                throw new PersonNotFoundException("Requested Person does not exist");
+            } else {
+                return new PersonDTO(person);
+            }
+        }finally{
+            em.close();
+        }
+        
+    }
+    
+    
+    
     
 }
